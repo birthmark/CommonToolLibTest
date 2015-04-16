8185132eef54dfb8176bd0527ff78254d2779d18
XiaoG <kongglin@163.com>
2015-04-16 11:51:25 +0800

    add code

---
+++ CommonToolLibTest/MainVC.h
@@ -0,0 +1,13 @@
+//
+//  MainVC.h
+//  CommonToolLibTest
+//
+//  Created by lingshan.kong on 15/4/16.
+//  Copyright (c) 2015年 XiaoG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainVC : UIViewController
+
+@end
---
+++ CommonToolLibTest/MainVC.m
@@ -0,0 +1,37 @@
+//
+//  MainVC.m
+//  CommonToolLibTest
+//
+//  Created by lingshan.kong on 15/4/16.
+//  Copyright (c) 2015年 XiaoG. All rights reserved.
+//
+
+#import "MainVC.h"
+
+@interface MainVC ()
+
+@end
+
+@implementation MainVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
